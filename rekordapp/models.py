3b123630cd1b9ddbf3ee4d5d86163d2c2a1d2e6a
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-14 20:29:19 +0530

    db created

--- rekordapp/models.py
+++ rekordapp/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
+class organization(models.Model):
+    id=models.AutoField(primary_key=True)   #automatic id
+    name=models.CharField(max_length=100,unique=True)   #name of the organization   
+    emails=models.EmailField(unique=True)   #email of the organization
+    password=models.CharField(max_length=30)    #passoword
+
+def __str__(self):
+    return self.name

d91d98e6466ce2209db1c0696666e32a62aaaf16
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-14 14:15:20 +0530

    signup and login creation

--- rekordapp/urls.py
+++ rekordapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[path("login/",views.login),
-             path("",views.home)]
+urlpatterns=[path("signup/",views.signup),
+             path("",views.login)]

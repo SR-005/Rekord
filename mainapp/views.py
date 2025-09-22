977a258ec91c39db5e1932f5123f8dbd278d1b56
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-22 22:12:14 +0530

    static files connected

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib import messages
 # Create your views here.
+
+def homepage(request):
+    return render(request,"homepage.html")

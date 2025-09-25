8326d926eaeb677a92657b6fdf3431ebee51a56b
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-25 22:19:40 +0530

    events db added

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+from rekordapp.models import organization
 
 # Create your models here.
-class organization(models.Model):
-    id=models.AutoField(primary_key=True)   #automatic id
-    name=models.CharField(max_length=100,unique=True)   #name of the organization   
-    email=models.EmailField(unique=True)   #email of the organization
-    password=models.CharField(max_length=30)    #passoword
-
+class events(models.Model):
+    eventid=models.AutoField(primary_key=True)
+    organizationid=models.ForeignKey(organization, on_delete=models.CASCADE,related_name="events") #use event.organization.fieldname to access foreign table data
+    eventname=models.CharField(max_length=100)
+    eventdescription=models.CharField(max_length=200)
+    eventdate=models.CharField(max_length=12)
+    #for saving icons. it doesnt save icon as img, it will automatically save it to /media/icons and only save it path in db.
+    #use 'event.eventicon.url' to access it
+    eventicon = models.ImageField(upload_to="icons/", null=True, blank=True) 
     def __str__(self):
-        return self.name
+        return self.eventname

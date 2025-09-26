e68841e25e74f3236dbd63fa71ec15af2b15686a
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-26 23:10:38 +0530

    Data fetched

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
 from rekordapp.models import organization
+from .forms import createeventForm
 # Create your views here.
 
 def homepage(request):
@@ -10,4 +11,11 @@ def homepage(request):
 
     if request.method=="POST":
         action=request.POST.get("action") #for pinpointing which button was clicked
+        if action=="create-event":
+            form=createeventForm(request.POST, request.FILES)
+            '''print(form)'''
+            if form.is_valid():
+                print("DATA:",form.cleaned_data)
+            else:
+                print("BUTTON WORKS BUT SOME FORM ERROR")
     return render(request,"homepage.html",{"organizationname":organizationdetails.name, "organizationemail":organizationdetails.email})

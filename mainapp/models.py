dcb9cfab0062a1b5bbad02ea0de601e4f7e55ccf
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-09-26 22:23:12 +0530

    image storage in sqlite3

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,6 +11,6 @@ class events(models.Model):
     eventdate=models.CharField(max_length=12)
     #for saving icons. it doesnt save icon as img, it will automatically save it to /media/icons and only save it path in db.
     #use 'event.eventicon.url' to access it
-    eventicon = models.ImageField(upload_to="icons/", null=True, blank=True) 
+    eventicon = models.ImageField(upload_to="icons/")
     def __str__(self):
         return self.eventname

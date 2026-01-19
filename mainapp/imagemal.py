f2d75ae914c3032512432d7c5830c1f411d447cc
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-01-19 20:08:43 +0530

    code optimization

--- mainapp/imagemal.py
+++ mainapp/imagemal.py
@@ -3,8 +3,6 @@ from PIL import Image,ImageDraw, ImageFilter, ImageFont
 def imagemanipulation(image):
     img=Image.open(image).convert("RGBA")                   #load the image
     nftsize=1024
-    prestige="flagship"
-    loyality="short"
 
     width,height=img.size                           #get the current dimentions
     mindimention=min(width,height)            #to get the minimum w and h without loosing quality and aspect ratio
@@ -22,102 +20,23 @@ def imagemanipulation(image):
     nftimage.save("nftimage.png")
     return nftimage
 
-def loyality(image,prestige,loyality):
-    nftimage=Image.open(str(image))
-    nftsize=1024
 
-    if loyality=="long":                                      #pixeled image section
-        thickness=3
-        pixelsize=5
-        maxcolors=64
-
-        pixelednft=nftimage.resize((nftsize//pixelsize, nftsize//pixelsize), resample=Image.BILINEAR)
-        colorednft=pixelednft.quantize(colors=maxcolors)
-        finalnft=colorednft.resize((nftsize,nftsize),resample=Image.NEAREST)
-
-    elif loyality in ["short","medium"]:  # colored dot image section
-        thickness = 0
-        dotspacing = 7
-        maxdotradius = 5
-
-        # Grayscale for brightness
-        gray = nftimage.convert("L")
-        gray_pixels = gray.load()
-
-        # White canvas
-        dotted = Image.new("RGBA", (nftsize, nftsize), "white")
-        dot_draw = ImageDraw.Draw(dotted)
-
-        for y in range(0, nftsize, dotspacing):
-            for x in range(0, nftsize, dotspacing):
-                brightness = gray_pixels[x, y]  # 0–255
-
-                # Darker pixel → bigger dot
-                radius = int((255 - brightness) / 255 * maxdotradius)
-
-                if radius > 0:
-                    dot_draw.ellipse(
-                        (
-                            x - radius,
-                            y - radius,
-                            x + radius,
-                            y + radius
-                        ),
-                        fill="black"
-                    )
-
-        finalnft = dotted
-
-    #border seciton
-    draw=ImageDraw.Draw(finalnft)
-    if prestige=="standard":
-        color="#46D12D"
-        thickness=thickness+8
-    elif prestige=="signature":
-        color="#F1C40F"
-        thickness=thickness+10
-    elif prestige=="flagship":
-        color="#8E44AD"
-        thickness=thickness+12
-
-    #glow section
-    glow_radius=3
-    glow_layer = Image.new("RGBA", (nftsize, nftsize), (0, 0, 0, 0))
-    glow_draw = ImageDraw.Draw(glow_layer)
-
-    for i in range(thickness):
-        glow_draw.rectangle(
-            [i, i, nftsize - i - 1, nftsize - i - 1],
-            outline=color
-        )
-
-    glow_layer = glow_layer.filter(ImageFilter.GaussianBlur(glow_radius))
-    finalnft = finalnft.convert("RGBA")
-    finalnft = Image.alpha_composite(finalnft, glow_layer)
-
-    for i in range(thickness):
-        draw.rectangle([i, i, nftsize-i-1, nftsize-i-1], outline=color)
-
-    finalnft.save("nftimage.png")
-    return 0
-
-
-
-def imagetest(image,prestige,loyality):
-
-    # Open image
+def prestige(image,prestige):
     img=Image.open(image).convert("RGB")
     imagesize=1024
     
     if prestige=="standard":
         bordercolor=(40,38,40)          #border color: grey-black
         textcolor = (255, 255, 255)     #text color: white
+        organizationnamecolor=(255, 255, 255)   #org name color: white
     elif prestige=="signature": 
         bordercolor=(252, 197, 33)      #border color: yellow-gold
         textcolor = (0, 0, 0)           #text color: black
+        organizationnamecolor=(143,56,197)      #org name color: purple
     elif prestige=="flagship":
         bordercolor=(143,56,197)        #border color: purple
         textcolor = (255, 255, 255)     #text color: white
+        organizationnamecolor=(0,0,0)   #org name color: black
     else:
         print("Event Prestige Not Valid!!")
 
@@ -151,8 +70,6 @@ def imagetest(image,prestige,loyality):
     canvas.paste(img, (borderleft, bordertop))
 
 
-
-
     # --- Draw text ---
     draw=ImageDraw.Draw(canvas)
     font=ImageFont.truetype("Roca_Two_Bold.ttf", 104)
@@ -172,9 +89,9 @@ def imagetest(image,prestige,loyality):
 
 
     font = ImageFont.truetype("Roca_Two_Bold.ttf", 58)
-    text = "by tinkerhub"
+    organizationname = "by tinkerhub"
     # Measure text size
-    bbox = draw.textbbox((0, 0), text, font=font)
+    bbox = draw.textbbox((0, 0), organizationname, font=font)
     text_w = bbox[2] - bbox[0]
     text_h = bbox[3] - bbox[1]
 
@@ -182,17 +99,44 @@ def imagetest(image,prestige,loyality):
     mainx = (imagesize - text_w) //1.03
     mainy =imagesize - borderbottom+ (borderbottom- text_h) // 2
     mainy+=40
-    draw.text((mainx, mainy), text, fill=textcolor, font=font)
+    draw.text((mainx, mainy), organizationname, fill=organizationnamecolor, font=font)
+
+    canvas.save("prestigeimage.png")
+
+def loyality(image,loyality):
+
+    #Open Image, Draw and Canvas
+    img=Image.open(image).convert("RGB")
+    imagesize=1024
+
+    # Borders
+    bordertop=40
+    borderleft=20
+    borderright=20
+    borderbottom=160    
+
+    # Image area
+    cropped_imagewidth=imagesize-(borderleft+ borderright)
+    cropped_imageheight=imagesize-(bordertop+ borderbottom)
+
+    canvas = Image.new("RGB", (imagesize, imagesize), (0, 0, 0))
+    canvas.paste(img)
+    draw = ImageDraw.Draw(canvas)
 
     #loyality levels
     if loyality=="short":
         gray_img = img.convert("L").convert("RGB")  
-        canvas.paste(gray_img, (borderleft, bordertop))
+        canvas.paste(gray_img)
 
     elif loyality=="medium":
         pass
 
     elif loyality=="long":
+
+        headercolor1=(143,56,197)        #header color: purple
+        headercolor2=(0,0,0)        #header color: purple
+        
+
         #draw bounding box for the image inside border
         x1=borderleft
         y1=bordertop
@@ -200,12 +144,11 @@ def imagetest(image,prestige,loyality):
         y2=bordertop+ cropped_imageheight
 
         image_height = y2 - y1
-        label = "REKORD FLAGSHIP NFT"
+        headertext1 = "REKORD FLAGSHIP NFT"
         font = ImageFont.truetype("Roca_Two_Bold.ttf", 22)
-        text_color = (0,0,0)
 
         # Measure text
-        bbox = draw.textbbox((0, 0), label, font=font)
+        bbox = draw.textbbox((0, 0), headertext1, font=font)
         text_w = bbox[2] - bbox[0]
         text_h = bbox[3] - bbox[1]
 
@@ -217,15 +160,13 @@ def imagetest(image,prestige,loyality):
         top_offset_ratio = 0.01 
         text_y = y1 + int(image_height * top_offset_ratio)
 
-        draw.text((text_x, text_y), label, fill=text_color, font=font)
+        draw.text((text_x, text_y), headertext1, fill=headercolor1, font=font)
 
-
-        label = "0xDFDa8340978B38d93114FAE615144e895A75ebb2"
+        headertext2 = "0xDFDa8340978B38d93114FAE615144e895A75ebb2"
         font = ImageFont.truetype("Roca_Two_Bold.ttf", 16)
-        text_color = (0,0,0)
 
         # Measure text
-        bbox = draw.textbbox((0, 0), label, font=font)
+        bbox = draw.textbbox((0, 0), headertext2, font=font)
         text_w = bbox[2] - bbox[0]
         text_h = bbox[3] - bbox[1]
 
@@ -237,20 +178,18 @@ def imagetest(image,prestige,loyality):
         top_offset_ratio = 0.01 
         text_y = y1 + int(image_height * top_offset_ratio)
 
-        draw.text((text_x, text_y), label, fill=text_color, font=font)
+        draw.text((text_x, text_y), headertext2, fill=headercolor2, font=font)
     else:
         print("Loyality level is Invalid!!")
 
     # Save
-    canvas.save("nftimage.png")
-
-
+    canvas.save("loyalityimage.png")
 
 
 if __name__ == "__main__":
     '''imagemanipulation("testimage3.png")'''
-    '''loyality("testimage3.png","flagship","short")'''
-    imagetest("testimage.png","flagship","short")
+    prestige("testimage.png","signature")
+    loyality("prestigeimage.png","long")
 #standard
 #signature
 #flagship
